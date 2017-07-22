2064fa36bf4b80f29b467a2d1d6c94014739ac2f
Kevin Jung <rubic22@gmail.com>
2017-07-21 21:38:33 -0700

    determineWinner written, untested

--- javasrc/Decision.java
+++ javasrc/Decision.java
@@ -21,6 +21,7 @@ class Decision {
         return Action.HIT;
     }
     
+    // Evals at highest possible value
     private int sumOfArrayList(ArrayList<Integer> in) {
         int total = 0;
         int aces = 0;
@@ -39,13 +40,44 @@ class Decision {
 
     // determineWinner() 
     // returns a multiplier for the amount won, x0, x1, x2, x2.5
-    public double determineWinner(ArrayList<Integer> userHand, ArrayList<Integer> dealerHand, boolean isDouble) {
-        // if double, return 2x vals
+    public double determineWinner(ArrayList<Integer> userHand, ArrayList<Integer> dealerHand) {
+        double ret;
+        int uHandStr, dHandStr; 
+        // 25 nat 21
+        // 24 5 card charlie
+        // sum for all else
+
+        // Handle double in main
         // Evaluate hand strength
+        int uSum = sumOfArrayList(userHand);
+        int dSum = sumOfArrayList(dealerHand);
+        if ((uSum == 21) && (userHand.size()==2)) {
+            uHandStr = 25;
+        } else if ((uSum <= 21) && (userHand.size()==5)) {
+            uHandStr = 24;
+        } else {
+            uHandStr = uSum;
+        }
+        if ((dSum == 21) && (dealerHand.size()==2)) {
+            dHandStr = 2;
+        } else if (dSum <= 21) && (dealerHand.size()==5)) {
+            dHandStr = 1;
+        } else {
+            dHandStr = dSum;
+        }
+
         // Determine who has stronger hand
         // if dealer, always 0
         // if tie ret 1
         // if user, return 2 or 2.5
-    	return 0;
+        if ((uHandStr == 25) && (dHandStr!=25)) {
+            return 2.5;
+        } else if (uHandStr > dHandStr) {
+            return 2.0;
+        } else if (uHandStr == dHandStr) {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
     }
 }
