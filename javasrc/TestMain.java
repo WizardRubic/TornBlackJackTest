101da1a0b8fbf1fa10c26939a04f76cddda39d8f
Kevin Jung <rubic22@gmail.com>
2017-07-22 21:34:07 -0700

    test file for determine winner

---
+++ javasrc/TestMain.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class TestMain {
+	public static void main(String[] args) {
+		ArrayList<Integer> utest = new ArrayList<Integer>();
+		ArrayList<Integer> dtest = new ArrayList<Integer>();
+		Decision decision = new Decision();
+		dtest.add(10);
+		dtest.add(11);
+		utest.add(10);
+		utest.add(7);
+		System.out.printf("expected 0| result - %s\n",decision.determineWinner(utest,dtest).toString());
+		// =====
+		dtest.clear();
+		utest.clear();
+		dtest.add(10);
+		dtest.add(11);
+		utest.add(2);
+		utest.add(2);
+		utest.add(2);
+		utest.add(2);
+		utest.add(3);
+		System.out.printf("expected 0| result - %s\n",decision.determineWinner(utest,dtest).toString());
+		// =====
+		dtest.clear();
+		utest.clear();
+		dtest.add(10);
+		dtest.add(10);
+		utest.add(2);
+		utest.add(2);
+		utest.add(2);
+		utest.add(2);
+		utest.add(3);
+		System.out.printf("expected 2.0| result - %s\n",decision.determineWinner(utest,dtest).toString());
+		// =====
+		dtest.clear();
+		utest.clear();
+		dtest.add(11);
+		dtest.add(11);
+		dtest.add(2);
+		dtest.add(2);
+		dtest.add(5);
+		utest.add(2);
+		utest.add(2);
+		utest.add(2);
+		utest.add(2);
+		utest.add(3);
+		System.out.printf("expected 1.0| result - %s\n",decision.determineWinner(utest,dtest).toString());
+		// =====
+		dtest.clear();
+		utest.clear();
+		dtest.add(11);
+		dtest.add(11);
+		dtest.add(2);
+		dtest.add(2);
+		dtest.add(5);
+		utest.add(10);
+		utest.add(11);
+		System.out.printf("expected 2.5| result - %s\n",decision.determineWinner(utest,dtest).toString());
+	}
+}
