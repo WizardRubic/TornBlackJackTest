77c44ba870889ef72957aceb947ec2b8093af706
Kevin Jung <rubic22@gmail.com>
2017-08-15 16:05:23 -0700

    refactored to use BigDecimal

--- javasrc/User.java
+++ javasrc/User.java
@@ -1,51 +1,61 @@
+import java.math.BigDecimal;
+
 class User {
-	private double cashTotal;
+	private BigDecimal cashTotal;
 	private int handsPlayed;
-	private double cashChanged;
-	protected double baseBet;
-	private double startCash;
+	private BigDecimal cashChanged;
+	protected BigDecimal baseBet;
+	private BigDecimal startCash;
 	public User() {
-		cashTotal = 0;
+		cashTotal = new BigDecimal(0);
 		handsPlayed = 0;
-		cashChanged = 0;
+		cashChanged = new BigDecimal(0);
 	}
 
 	public User(double startCash) {
-		cashTotal = startCash;
-		this.startCash = startCash;
+		cashTotal = new BigDecimal(startCash);
+		this.startCash = new BigDecimal(startCash);
 		handsPlayed = 0;
-		cashChanged = 0;
+		cashChanged = new BigDecimal(0.0);
 	}
 
-	public double getCash() {
+	public BigDecimal getCash() {
 		return cashTotal;
 	}
-	public void setCash(double amt) {
+	public void setCash(BigDecimal amt) {
 		cashTotal = amt;
 	}
-	public void changeCash(double delta) {
-		cashChanged += delta;
-		cashTotal += delta;
+	public void changeCash(BigDecimal delta) {
+		cashChanged = cashChanged.add(delta);
+		cashTotal = cashTotal.add(delta);
 	}
 	public void incrementHandsPlayed() {
 		handsPlayed++;
 	}
-	public double getHandsPlayed() {
+	public int getHandsPlayed() {
 		return handsPlayed;
 	}
-	public double getProfitPerHand() {
-		return cashChanged / handsPlayed;
+	public BigDecimal getProfitPerHand() {
+		return cashChanged.divide(new BigDecimal(handsPlayed));
 	}
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("UserStats:\n");
+		sb.append("Number of hands: ");
+		sb.append(getHandsPlayed());
+		sb.append("\n");
 		sb.append("Profit per hand: ");
 		sb.append(getProfitPerHand());
 		sb.append("\n");
+		sb.append("startCash: ");
+		sb.append(startCash);
+		sb.append("\nendCash: ");
+		sb.append(cashTotal);
+		sb.append("\n");
 		return sb.toString();
 	}
-	public double getBaseBet() {
-		return 20.0;
+	public BigDecimal getBaseBet() {
+		return baseBet;
 	}
 	public void updateBaseBet() {
 		return;
